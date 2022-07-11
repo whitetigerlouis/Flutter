15a7c2cedf8236aecb1ba85b0c783b11324c01ad
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-07-11 22:06:57 +0900

    Create 22.dart

---
+++ Coding_Chef/22.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// custom widget
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Appbar',
+
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the Second page'),
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(
+                builder: (BuildContext context) => SecondPage()));
+          },),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Go to the First page'),
+          onPressed: (){
+            Navigator.pop(ctx);
+          },),
+      ),
+    );
+  }
+}

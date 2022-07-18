e6905918c85e4a9743a797cd0780433e18cd3cb4
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-07-18 14:52:19 +0900

    Update profile_buttons.dart

--- Flutter_UI/06_flutter_profile/lib/components/profile_buttons.dart
+++ Flutter_UI/06_flutter_profile/lib/components/profile_buttons.dart
@@ -12,11 +12,46 @@ class ProfileButtons extends StatelessWidget {
     );
   }
 
-  Widget _buildFollowButton(){
-    return SizedBox();
+  Widget _buildFollowButton() {
+    return InkWell(
+      onTap: () {
+        print("Follow 버튼 클릭됨");
+      },
+      child: Container(
+        alignment: Alignment.center, // 컨네이너 내부
+        width: 150,
+        height: 45,
+        child: Text(
+          "Follow",
+          style: TextStyle(color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10), // 컨테이너 모서리를 둥글게 한다
+        ),
+      ),
+    );
   }
 
-  Widget _buildMessageButton(){
-    return SizedBox();
+  Widget _buildMessageButton() {
+    return InkWell(
+      onTap: () {
+        print("Message 버튼 클릭됨");
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: 150,
+        height: 45,
+        child: Text(
+          "Message",
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(), // 컨테이너에 테두리 선을 준다.
+        ),
+      ),
+    );
   }
 }

14d9409f2def7a320d8cf1fd9e950097b04265a8
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-07-18 15:05:23 +0900

    Update profile_header.dart

--- Flutter_UI/06_flutter_profile/lib/components/profile_header.dart
+++ Flutter_UI/06_flutter_profile/lib/components/profile_header.dart
@@ -13,7 +13,7 @@ class ProfileHeader extends StatelessWidget {
     );
   }
 
-  Widget _buildHeaderAvatar(){
+  Widget _buildHeaderAvatar() {
     return SizedBox(
       width: 100,
       height: 100,
@@ -23,7 +23,7 @@ class ProfileHeader extends StatelessWidget {
     );
   }
 
-  Widget _buildHeaderProfile(){
+  Widget _buildHeaderProfile() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
